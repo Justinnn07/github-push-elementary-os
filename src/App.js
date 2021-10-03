bb099d36741941cb945684056db331dfca8be205
Justinnn07 <justinv2006@gmail.com>
2021-10-03 16:28:06 +0500

    initial commit :fire:

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+const App = () => {
+  return (
+    <div>
+      <AccountCircle />
+    </div>
+  );
+};
+
+export default App;
